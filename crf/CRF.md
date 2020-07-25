216c84ba3d9e8c612131e2cffdf5d93e054a504f
bannima <zhouenguo@163.com>
2020-07-25 16:57:40 +0800

    update CRF

---
+++ crf/CRF.md
@@ -0,0 +1,5 @@
+
+
+
+###参考资料
+1.[Introduction to Conditional Random Fields](http://blog.echen.me/2012/01/03/introduction-to-conditional-random-fields/)(强烈推荐)
---
+++ crf/test/test_crf.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+"""
+FileName: test_crf.py
+Description: 
+Author: Barry Chow
+Date: 2020/5/31 10:33 PM
+Version: 0.1
+"""
